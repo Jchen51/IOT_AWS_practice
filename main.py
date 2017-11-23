81e8ebe93d4d3586e30d05bb6b2ed911c01bd933
Jchen51 <jasonthechen@gmail.com>
2017-11-22 18:48:54 -0800

    adding aws

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ camera.hflip = True
 camera.vflip = True
 
 import boto3
-
-s3 = boto3.resource('s3')
+import json
 
 #
 # See http://www.wrightfully.com/garagepi-my-raspberry-pi-playground
@@ -21,7 +20,7 @@ s3 = boto3.resource('s3')
 GPIO.setmode(GPIO.BCM)
 
 
-SPEED_OF_SOUND = 34000 #cm/s    
+SPEED_OF_SOUND = 34000 #cm/s
 NOTIFY = 70 #cm  - is actually about 60 but get readings up to 68 sometimes
 SAMPLE_SPEED = 5 #seconds
 
@@ -34,19 +33,19 @@ LED_RUN = 25
 class SonicController:
   def readDistance(self):
 
-    print "Distance Measurement In Progress " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())    
-    
+    print "Distance Measurement In Progress " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
-    
+
     pulse_start = time.time()
     while GPIO.input(ECHO)==0:
       pulse_start = time.time()
-    
+
     while GPIO.input(ECHO)==1:
-      pulse_end = time.time()      
-    
+      pulse_end = time.time()
+
     pulse_duration = pulse_end - pulse_start
     roundtrip_duration = pulse_duration * SPEED_OF_SOUND
     one_way_distance = roundtrip_duration/2
@@ -54,14 +53,14 @@ class SonicController:
     return one_way_distance
 
   def init(self):
-    
+
     print "Initializing Ultrasonic Range Finder"
-    
+
     #GPIO.setup(TRIG, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
     GPIO.setup(TRIG, GPIO.OUT)
     #GPIO.setup(ECHO, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
     GPIO.setup(ECHO, GPIO.IN)
-    
+
     GPIO.output(TRIG, False)
     print "Waiting For Sensor To Settle"
     time.sleep(2)
@@ -91,7 +90,7 @@ class LedController:
     GPIO.output(LED_RUN, False)
 
 led = LedController()
-led.init() 
+led.init()
 
 sensor = SonicController()
 sensor.init()
@@ -105,32 +104,58 @@ try:
     distance = sensor.readDistance()
     if distance < NOTIFY:
       print "    Take picture"
-      
+
       #find current date and time
       str1 = datetime.datetime.now()
       str1 = str1.strftime('%m_%d_%Y_%H_%M_%S')
       str2 = "/home/pi/Desktop/"
       str3 = ".jpg"
       str1 = str2 + str1
-      filename = str1 + str3
-      
+
+      #using set jpg name for now
+      #filename = str1 + str3
+      filename = "newPhoto.jpg"
+
       #take picture
       camera.start_preview()
       sleep(5)
       camera.capture(filename)
       camera.stop_preview()
-      
-      #AWS
+
+      ############################AWS########################################
+
+      s3 = boto3.resource('s3')
+
+      #Bucket Name
       bucketName = 'tchen44-test'
-      photo = open(fileName, 'rb')
-      keyName = fileName +'-'+ datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-      s3.Bucket(bucketName).put_object(Key=keyName,Body=photo )
-      
+
+      #Upload Photo
+      photo = open(filename, 'rb')
+      s3.Bucket(bucketName).put_object(Key=filename,Body=photo )
+
+      #simple recognition
+      #'''
+
+      fileName='newPhoto.jpg'
+      client=boto3.client('rekognition','us-west-2')
+
+      response = client.detect_faces(Image={'S3Object':{'Bucket':bucketName,'Name':fileName}},Attributes=['ALL'])
+
+      print('Detected faces for ' + fileName)
+      for faceDetail in response['FaceDetails']:
+          print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
+              + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
+          print('Here are the other attributes:')
+          print(json.dumps(faceDetail, indent=4, sort_keys=True))
+      #'''
+
+      ########################################################################
+
       #led.turnOnDoorLed()
     else:
       print "    Nothing too close"
       #led.turnOffDoorLed()
-    
+
     time.sleep(SAMPLE_SPEED)
 except KeyboardInterrupt:
     print "keyboard interrupt caught"
